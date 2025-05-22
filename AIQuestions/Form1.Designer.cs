60fd6d06dd2dcec619cc14b8a22a13402af0ff52
Steven Tharwat <stevven.helmy@gmail.com>
2025-05-22 20:11:58 +0300

    V1.0

---
+++ AIQuestions/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace AIQuestions
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSigns = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.labStatus = new System.Windows.Forms.Label();
+            this.labScore = new System.Windows.Forms.Label();
+            this.labAnswer = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSigns
+            // 
+            this.txtSigns.Location = new System.Drawing.Point(76, 70);
+            this.txtSigns.Name = "txtSigns";
+            this.txtSigns.Size = new System.Drawing.Size(229, 23);
+            this.txtSigns.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Signs";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Question";
+            // 
+            // txtQuestion
+            // 
+            this.txtQuestion.Enabled = false;
+            this.txtQuestion.Location = new System.Drawing.Point(76, 115);
+            this.txtQuestion.Name = "txtQuestion";
+            this.txtQuestion.Size = new System.Drawing.Size(229, 23);
+            this.txtQuestion.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Answer";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(76, 162);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(229, 23);
+            this.txtAnswer.TabIndex = 6;
+            this.txtAnswer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAnswer_KeyPress);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(76, 209);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(134, 23);
+            this.btnCheck.TabIndex = 8;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnChange
+            // 
+            this.btnChange.Location = new System.Drawing.Point(76, 246);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(134, 23);
+            this.btnChange.TabIndex = 9;
+            this.btnChange.Text = "Change Question";
+            this.btnChange.UseVisualStyleBackColor = true;
+            this.btnChange.Click += new System.EventHandler(this.btnChange_Click);
+            // 
+            // labStatus
+            // 
+            this.labStatus.AutoSize = true;
+            this.labStatus.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labStatus.Location = new System.Drawing.Point(232, 222);
+            this.labStatus.Name = "labStatus";
+            this.labStatus.Size = new System.Drawing.Size(73, 30);
+            this.labStatus.TabIndex = 10;
+            this.labStatus.Text = "Status";
+            // 
+            // labScore
+            // 
+            this.labScore.AutoSize = true;
+            this.labScore.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labScore.ForeColor = System.Drawing.Color.Green;
+            this.labScore.Location = new System.Drawing.Point(76, 9);
+            this.labScore.Name = "labScore";
+            this.labScore.Size = new System.Drawing.Size(106, 30);
+            this.labScore.TabIndex = 11;
+            this.labScore.Text = "Score = 0";
+            // 
+            // labAnswer
+            // 
+            this.labAnswer.AutoSize = true;
+            this.labAnswer.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labAnswer.Location = new System.Drawing.Point(54, 283);
+            this.labAnswer.Name = "labAnswer";
+            this.labAnswer.Size = new System.Drawing.Size(86, 30);
+            this.labAnswer.TabIndex = 12;
+            this.labAnswer.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 322);
+            this.Controls.Add(this.labAnswer);
+            this.Controls.Add(this.labScore);
+            this.Controls.Add(this.labStatus);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtQuestion);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSigns);
+            this.Name = "Form1";
+            this.Text = "AI Questions";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox txtSigns;
+        private Label label1;
+        private Label label2;
+        private TextBox txtQuestion;
+        private Label label3;
+        private TextBox txtAnswer;
+        private Button btnCheck;
+        private Button btnChange;
+        private Label labStatus;
+        private Label labScore;
+        private Label labAnswer;
+    }
+}
---
+++ AIQuestions/Form1.cs
@@ -0,0 +1,123 @@
+﻿namespace AIQuestions
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> _questionsDic = new Dictionary<string, string>();
+        List<string> _questionsList = new List<string>();
+        string _question = string.Empty;
+        Random r = new Random();
+        int score;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            txtSigns.Text = "(     )     p     q     ^     ∨";
+            labAnswer.Text = "";
+            fillDic();
+            getRandomQuestion();
+            txtAnswer.Focus();
+        }
+
+        private void getRandomQuestion()
+        {
+            txtAnswer.Text = string.Empty;
+            _question = _questionsList[r.Next(0, _questionsList.Count - 1)];
+            txtQuestion.Text = _question;
+
+            //labStatus.Text = "Status";
+            //labStatus.ForeColor = Color.Black;
+        }
+
+        private void fillDic()
+        {
+            _questionsDic.Add("P^T","P");
+            _questionsDic.Add("P∨F","P");
+            _questionsDic.Add("P∨T","T");
+            _questionsDic.Add("P^F","F");
+            _questionsDic.Add("P∨P","P");
+            _questionsDic.Add("P^P","P");
+            _questionsDic.Add("P∨~P","T");
+            _questionsDic.Add("P^~P","F");
+            _questionsDic.Add("~(P^Q)","~p∨~Q");
+            _questionsDic.Add("~(P∨Q)","~p^~Q");
+            _questionsDic.Add("p^(p∨q)","p");
+            _questionsDic.Add("p∨(p^q)","p");
+            _questionsDic.Add("p^q","q^p");
+            _questionsDic.Add("p∨q","q∨p");
+            _questionsDic.Add("(p∨q)^r","(r^p)∨(r^q)");
+            _questionsDic.Add("(p^q)∨r","(r∨p)^(r∨q)");
+            _questionsDic.Add("(p^q)^r","p^(q^r)");
+            _questionsDic.Add("(p∨q)∨r","p∨(q∨r)");
+
+            _questionsDic.Add("~p∨~Q","~(P^Q)");
+            _questionsDic.Add("~p^~Q","~(P∨Q)");
+            _questionsDic.Add("q^p","p^q");
+            _questionsDic.Add("q∨p","p∨q");
+            _questionsDic.Add("(r^p)∨(r^q)","(p∨q)^r");
+            _questionsDic.Add("(r∨p)^(r∨q)","(p^q)∨r");
+            _questionsDic.Add("p^(q^r)","(p^q)^r");
+            _questionsDic.Add("p∨(q∨r)","(p∨q)∨r");
+
+            _questionsList =_questionsDic.Keys.ToList();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string answer = _questionsDic[_question];
+            if (answer.Trim().ToLower() == txtAnswer.Text.Trim().ToLower())
+            {
+                score += 1;
+                labScore.Text = $"Score = {score}";
+                labStatus.Text = "Right";
+                labStatus.ForeColor = Color.Green;
+                labScore.ForeColor = Color.Green;
+                labAnswer.Text = "";
+                getRandomQuestion();
+            }
+            else
+            {
+                score -= 1;
+                labScore.Text = $"Score = {score}";
+                labStatus.Text = "Wrong";
+                labStatus.ForeColor = Color.Red;
+                labScore.ForeColor = Color.Red;
+                labAnswer.Text = $"The Answer is : {answer}";
+            }
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            score -= 1;
+            labScore.Text = $"Score = {score}";
+            labScore.ForeColor = Color.Red;
+            getRandomQuestion();
+        }
+
+        private void txtAnswer_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                string answer = _questionsDic[_question];
+                if (answer.Trim().ToLower() == txtAnswer.Text.Trim().ToLower())
+                {
+                    score += 1;
+                    labScore.Text = $"Score = {score}";
+                    labStatus.Text = "Right";
+                    labStatus.ForeColor = Color.Green;
+                    labScore.ForeColor = Color.Green;
+                    labAnswer.Text = "";
+                    getRandomQuestion();
+                }
+                else
+                {
+                    score -= 1;
+                    labScore.Text = $"Score = {score}";
+                    labStatus.Text = "Wrong";
+                    labStatus.ForeColor = Color.Red;
+                    labScore.ForeColor = Color.Red;
+                    labAnswer.Text = $"The Answer is : {answer}";
+                }
+            }
+        }
+    }
+}
